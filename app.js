565961a5b3dedb9a8f730d80422b6f9e0b6a40e5
Ian <istuart20@gmail.com>
2024-10-03 15:18:07 -0700

    Set up file structure, create user routes

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const indexRouter = require("./routes/index");
 
 const app = express();
-app.use(express.json());
 const { PORT = 3001 } = process.env;
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch(console.error);
+
+app.use("/", indexRouter);
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
