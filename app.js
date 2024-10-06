a95bb7de51c3efaaace579c811f14b65564a362c
Ian <istuart20@gmail.com>
2024-10-06 15:01:58 -0700

    clothingItem Schema and error logging

--- app.js
+++ app.js
@@ -12,7 +12,11 @@ mongoose
   })
   .catch(console.error);
 
+const routes = require("./routes");
 app.use(express.json());
+
+app.use(routes);
+
 app.use("/", indexRouter);
 
 app.listen(PORT, () => {
