c8d1b379ca7738227fc65fc1b41b586e2c481b99
Ian <istuart20@gmail.com>
2024-10-09 17:14:32 -0700

    remove curly brackets on const owner

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -9,7 +9,7 @@ const {
 const createItem = (req, res) => {
   const { name, weather, imageUrl } = req.body;
 
-  const { owner } = req.user._id;
+  const owner = req.user._id;
 
   ClothingItem.create({ name, weather, imageUrl, owner })
     .then((item) => {
