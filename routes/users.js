a435676ec9a93275000e448089d513dc457718d7
Ian <istuart20@gmail.com>
2025-01-13 15:41:25 -0800

    middleware creation

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const { getCurrentUser, updateUser } = require("../controllers/users");
 const { validateUpdateUser } = require("../middleware/validation");
+const auth = require("../middleware/auth");
 
-router.get("/me", getCurrentUser);
+router.get("/me", auth, getCurrentUser);
 router.patch("/me", validateUpdateUser, updateUser);
 
 module.exports = router;
