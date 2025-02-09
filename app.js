ba56df76fd3e8ca472493c0b6e369ddd12c3dd06
Ian <istuart20@gmail.com>
2025-02-09 10:55:00 -0800

    insert path

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const { errors } = require("celebrate");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
+const path = require("path");
 const indexRouter = require("./routes/index");
 const errorHandler = require("./middleware/error-handler");
 const { requestLogger, errorLogger } = require("./middleware/logger");
@@ -21,6 +22,7 @@ mongoose
 
 app.use(express.json());
 app.use(requestLogger);
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use(errorLogger);
 app.use(errors());
