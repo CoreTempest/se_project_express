a0fb81f6a3ac8f4599b4a8a32d8949a87db6214b
Ian <istuart20@gmail.com>
2025-02-09 10:17:23 -0800

    stuff

--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,5 @@
-const { JWT_SECRET = "some-secret-key" } = process.env;
+const {
+  JWT_SECRET = "55800af61afca1a7d19498074e01511d435dd0563a0c3cbd83748d8c2829cf43",
+} = process.env;
 
 module.exports = { JWT_SECRET };
